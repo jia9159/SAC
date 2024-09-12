e1b8ca361758f894ffa0ca0fb4bb4f8073d09828
jia9159 <jia9159@gmail.com>
2024-09-12 12:55:34 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -13,6 +13,22 @@ super()
 this._shadowRoot = this.attachShadow({ mode: 'open' })
 this._shadowRoot.appendChild(template.content.cloneNode(true))
 this._root = this._shadowRoot.getElementById('root')
+ onCustomWidgetResize (width, height){
+  this.render()
+ }
+
+ onCustomWidgetBeforeUpdate (changedProps){
+ }
+
+ onCustomWidgetAfterUpdate (){
+ }
+
+ onCustomWidgetDestroy (){
+ }
+
+ render (){
+  this._root.textContent = 'Hello Custom Widget clientWidth: ${this.clientWidth}, clientHeight: ${this.clientHeight}'
+ }
 }
 }
 customElements.define('com-sap-sac-exercise-mgs01-main', Main)
